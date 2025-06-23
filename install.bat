2a6d9d0e7b8652d72783afb44a5c8027f44b4a45
ovax <181485458+banaxou@users.noreply.github.com>
2025-06-23 16:56:49 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,10 @@
 @echo off 
 
 pip install instaloader
-pip install getpass
 pip install requests
 pip install fade
 pip install phonenumbers
 pip install pycountry 
-pip install json
 
 python hostagram.py
 pause
