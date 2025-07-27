fbab480ba6370fe3d5dd9369bb1f4edd4ac41eec
ovax <181485458+banaxou@users.noreply.github.com>
2025-07-27 12:26:27 +0200

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install python3-pip python3-venv -y
+python3 -m venv ovax
+source ovax/bin/activate
+pip install instaloader
+pip install requests
+pip install fade
+pip install phonenumbers
+pip install pycountry
+
+python3 ./hostagram.py
